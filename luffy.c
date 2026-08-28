84a40722bcfa0c7b05bb5b18c2bb35c42704a15c
praveen-vannur <vannurappagaripraveen6301@gmail.com>
2026-08-28 12:19:44 +0530

    Add files via upload

--- luffy.c
+++ luffy.c
@@ -1,8 +1,24 @@
-#include>stdio.h>
-#include<stdlib.h>
-struct node{
+#include<stdio.h>
+#include<stdlib>
+struct node {
 int data;
-struct node * prev;
-struct node * next;
+struct node *next;
 };
-struct node * head = NULL
+int main(){
+struct node *head = NULL;
+struct node *a = malloc(sizeof(struct node));
+struct node *b = malloc(sizeof(struct node));
+struct node *c = malloc(sizeof(struct node));
+a->data = 10;
+b->data = 20;
+c->data = 30;
+a->next = b;
+b->next = c;
+c->next = NULL;
+head = a;
+
+struct node *temp = head;
+while(temp != NULL){
+printf("%d", temp->data);
+temp = temp->next;}
+}
